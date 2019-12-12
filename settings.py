3262abdc140508734510973a5170beda489e370c
thedevluffy <52121827+TheDevLuffy@users.noreply.github.com>
2019-12-12 13:04:14 +0900

    enhancement/3/submodule (#6)
    * chore: update .gitignore
    
    * refacotr: replace local .env to submoduled .env
    
    * chore: add submodule settings-tecobrary-slack

--- settings.py
+++ settings.py
@@ -2,8 +2,13 @@ import os
 
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv(dotenv_path="./settings-tecobrary-slack/.env")
 
-AUTH_TOKEN = os.getenv("SLACK_AUTH_TOKEN")
-REQUESTED_CHANNEL = os.getenv("REQUESTED_CHANNEL")
-ENROLLED_CHANNEL = os.getenv("ENROLLED_CHANNEL")
+if os.getenv("ENVIRONMENT") == "test":
+    AUTH_TOKEN = os.getenv("TEST_SLACK_AUTH_TOKEN")
+    REQUESTED_CHANNEL = os.getenv("TEST_REQUESTED_CHANNEL")
+    ENROLLED_CHANNEL = os.getenv("TEST_ENROLLED_CHANNEL")
+elif os.getenv("ENVIRONMENT") == "deploy":
+    AUTH_TOKEN = os.getenv("DEPLOY_SLACK_AUTH_TOKEN")
+    REQUESTED_CHANNEL = os.getenv("DEPLOY_REQUESTED_CHANNEL")
+    ENROLLED_CHANNEL = os.getenv("DEPLOY_ENROLLED_CHANNEL")
